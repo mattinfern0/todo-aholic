ba09faeaad5d5e8a82f18c18f910b1e5ab334dd2
mattinfern0 <mattnfernandez@gmail.com>
2023-07-24 22:18:33 -0700

    updated schema

--- db/initial-schema.sql
+++ db/initial-schema.sql
@@ -1,13 +1,17 @@
-CREATE TABLE IF NOT EXISTS "user"
+CREATE SEQUENCE user_id_seq;
+
+CREATE TABLE IF NOT EXISTS app_user
 (
-    id       BIGSERIAL
+    id       BIGINT DEFAULT NEXTVAL('user_id_seq'::regclass) NOT NULL
         CONSTRAINT user_pk
             PRIMARY KEY,
-    email    VARCHAR(256) NOT NULL
+    email    VARCHAR(256)                                    NOT NULL
         CONSTRAINT user_unique_email
             UNIQUE,
-    password VARCHAR(100) NOT NULL
+    password VARCHAR(100)                                    NOT NULL
 );
+
+ALTER SEQUENCE user_id_seq OWNED BY app_user.id;
 
 CREATE TABLE IF NOT EXISTS task_list
 (
@@ -17,24 +21,45 @@ CREATE TABLE IF NOT EXISTS task_list
     display_name VARCHAR(100) NOT NULL,
     owner_id     BIGINT       NOT NULL
         CONSTRAINT task_list_owner_id_fk
-            REFERENCES "user"
+            REFERENCES app_user
 );
 
 CREATE TABLE IF NOT EXISTS task
 (
-    id            BIGSERIAL
+    id           BIGSERIAL
         CONSTRAINT task_pk
             PRIMARY KEY,
-    display_name  VARCHAR(100) NOT NULL,
-    owner_id      BIGINT       NOT NULL
+    display_name VARCHAR(100) NOT NULL,
+    owner_id     BIGINT       NOT NULL
         CONSTRAINT task_owner_id_fk
-            REFERENCES "user",
-    task_list_id  BIGINT
+            REFERENCES app_user,
+    task_list_id BIGINT
         CONSTRAINT task_task_list_id_fk
             REFERENCES task_list,
-    description   VARCHAR(255) NOT NULL,
-    "dueAt"       TIMESTAMP    NOT NULL,
-    "completedAt" TIMESTAMP
+    description  VARCHAR(255) NOT NULL,
+    due_at       TIMESTAMP,
+    completed_at TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS authority
+(
+    internal_name VARCHAR(50) NOT NULL
+        CONSTRAINT authority_unique_internal_name
+            UNIQUE,
+    id            BIGSERIAL
+        CONSTRAINT authority_pk
+            PRIMARY KEY
+);
+
+CREATE TABLE IF NOT EXISTS user_authorities
+(
+    id           BIGSERIAL
+        CONSTRAINT user_authorities_pk
+            PRIMARY KEY,
+    user_id      BIGINT NOT NULL,
+    authority_id BIGINT NOT NULL,
+    CONSTRAINT user_authorities_unique_user_authority
+        UNIQUE (user_id, authority_id)
 );
 
 
