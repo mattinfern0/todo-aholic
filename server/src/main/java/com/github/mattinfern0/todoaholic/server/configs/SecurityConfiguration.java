7123d285d1a63d09f096153a40919ebb00250929
mattinfern0 <mattnfernandez@gmail.com>
2023-09-22 01:25:09 -0700

    WIP: moved security config into configs folder

--- server/src/main/java/com/github/mattinfern0/todoaholic/server/configs/SecurityConfiguration.java
+++ server/src/main/java/com/github/mattinfern0/todoaholic/server/configs/SecurityConfiguration.java
@@ -1,4 +1,4 @@
-package com.github.mattinfern0.todoaholic.server;
+package com.github.mattinfern0.todoaholic.server.configs;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -6,14 +6,13 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-public class AppConfig {
+public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
