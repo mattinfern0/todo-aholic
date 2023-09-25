4392ab1f025afcd28ed725bd4b91afcb2b4bc1c8
mattinfern0 <mattnfernandez@gmail.com>
2023-09-24 19:19:00 -0700

    WIP: api client

---
+++ react-app/src/apis/backend/apiClient.ts
@@ -0,0 +1,57 @@
+import { backendApiConfig } from "../../configs.ts";
+import { BackendApiConnectionError, BackendApiError, UserDetails, UserDetailsSchema } from "./types.ts";
+
+const DEFAULT_FETCH_OPTIONS: RequestInit = {
+  method: "GET",
+};
+
+export const login = async (email: string, password: string): Promise<UserDetails> => {
+  const res = await apiFetch("/auth/login", {
+    method: "POST",
+    body: JSON.stringify({
+      email,
+      password,
+    }),
+  });
+
+  if (!res.ok) {
+    throw new BackendApiError();
+  }
+
+  return UserDetailsSchema.parse(await res.json());
+};
+
+export const logout = async (): Promise<void> => {
+  await apiFetch("/auth/logout", {
+    method: "POST",
+  });
+};
+
+export const getCurrentUserDetails = async (): Promise<UserDetails> => {
+  const res = await apiFetch("/users/me");
+
+  if (!res.ok) {
+    throw new BackendApiError();
+  }
+
+  return UserDetailsSchema.parse(await res.json());
+};
+
+const apiFetch = async (url: string, fetchOptions?: RequestInit) => {
+  const requestUrl = new URL(url, backendApiConfig.baseUrl);
+
+  try {
+    return await fetch(requestUrl, {
+      ...DEFAULT_FETCH_OPTIONS,
+      ...fetchOptions,
+    });
+  } catch (error: unknown) {
+    let message = "Unknown error";
+
+    if (error instanceof Error) {
+      message = error.message ?? message;
+    }
+
+    throw new BackendApiConnectionError(message);
+  }
+};
---
+++ react-app/src/apis/backend/queries.ts
@@ -0,0 +1,7 @@
+import { useQuery } from "@tanstack/react-query";
+import * as backendAPI from "./apiClient.ts";
+import { queryKeys } from "./queryKeys.ts";
+
+export const useCurrentUserDetailsQuery = () => {
+  return useQuery(queryKeys.currentUser(), async () => await backendAPI.getCurrentUserDetails());
+};
