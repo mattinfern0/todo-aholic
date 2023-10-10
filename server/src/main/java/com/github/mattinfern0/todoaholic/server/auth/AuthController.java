d12408f675871f38ca928bc93a973ef74b4f5693
mattinfern0 <mattnfernandez@gmail.com>
2023-10-09 19:52:10 -0700

    WIP: error handling and login errors

--- server/src/main/java/com/github/mattinfern0/todoaholic/server/auth/AuthController.java
+++ server/src/main/java/com/github/mattinfern0/todoaholic/server/auth/AuthController.java
@@ -2,15 +2,20 @@ package com.github.mattinfern0.todoaholic.server.auth;
 
 import com.github.mattinfern0.todoaholic.server.auth.dtos.LoginRequestDto;
 import com.github.mattinfern0.todoaholic.server.common.entities.User;
-import com.github.mattinfern0.todoaholic.server.users.dtos.UserDto;
 import com.github.mattinfern0.todoaholic.server.users.mappers.UserDtoMapper;
-import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
-import jakarta.validation.Valid;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.context.SecurityContextHolderStrategy;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
+import org.springframework.security.web.context.SecurityContextRepository;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,19 +24,30 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
+    private final AuthenticationManager authenticationManager;
+    private final SecurityContextRepository securityContextRepository =
+            new HttpSessionSecurityContextRepository();
+
+    private final SecurityContextHolderStrategy securityContextHolderStrategy = SecurityContextHolder.getContextHolderStrategy();
+
     private final UserDtoMapper userDtoMapper;
 
     @Autowired
-    public AuthController(UserDtoMapper userDtoMapper) {
+    public AuthController(AuthenticationManager authenticationManager, UserDtoMapper userDtoMapper) {
+        this.authenticationManager = authenticationManager;
         this.userDtoMapper = userDtoMapper;
     }
 
     @PostMapping("/login")
-    public ResponseEntity<?> login(
-        @RequestBody @Valid LoginRequestDto loginRequestDto,
-        HttpServletRequest httpServletRequest
-    ) throws ServletException {
-        httpServletRequest.login(loginRequestDto.getEmail(), loginRequestDto.getPassword());
+    public ResponseEntity<?> login(@RequestBody LoginRequestDto loginRequest, HttpServletRequest request, HttpServletResponse response) {
+        UsernamePasswordAuthenticationToken token = UsernamePasswordAuthenticationToken.unauthenticated(
+                loginRequest.getEmail(), loginRequest.getPassword());
+        Authentication authentication = authenticationManager.authenticate(token);
+        SecurityContext context = securityContextHolderStrategy.createEmptyContext();
+        context.setAuthentication(authentication);
+        securityContextHolderStrategy.setContext(context);
+        securityContextRepository.saveContext(context, request, response);
+
         User currentUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         return new ResponseEntity<>(userDtoMapper.userToUserDto(currentUser), HttpStatus.OK);
     }
--- server/src/main/java/com/github/mattinfern0/todoaholic/server/configs/SecurityConfiguration.java
+++ server/src/main/java/com/github/mattinfern0/todoaholic/server/configs/SecurityConfiguration.java
@@ -5,12 +5,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.authentication.logout.HttpStatusReturningLogoutSuccessHandler;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -29,23 +32,25 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
-            .cors(Customizer.withDefaults())
-            .formLogin().disable()
-            .csrf().disable()
-            .logout(logout -> logout
-                .logoutUrl("/auth/logout")
+                .cors(Customizer.withDefaults())
+                .formLogin().disable()
+                .csrf().disable()
+                .exceptionHandling()
+                .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+                .and()
+                .logout(logout -> logout
+                        .logoutUrl("/auth/logout")
 
-                // Disable redirecting to /login after successful logout
-                .logoutSuccessHandler(new HttpStatusReturningLogoutSuccessHandler(HttpStatus.OK))
-            )
-            .authorizeHttpRequests(authCustomizer ->
-                authCustomizer
-                    .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
-                    .requestMatchers( HttpMethod.POST, "/users").permitAll()
-                    .requestMatchers("/error").permitAll()
-                    .anyRequest().authenticated()
-            )
-            .build();
+                        // Disable redirecting to /login after successful logout
+                        .logoutSuccessHandler(new HttpStatusReturningLogoutSuccessHandler(HttpStatus.OK))
+                )
+                .authorizeHttpRequests(authCustomizer ->
+                        authCustomizer
+                                .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
+                                .requestMatchers(HttpMethod.POST, "/users").permitAll()
+                                .anyRequest().authenticated()
+                )
+                .build();
     }
 
     @Bean
@@ -57,7 +62,7 @@ public class SecurityConfiguration {
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(this.getAllowedOrigins());
-        configuration.setAllowedMethods(Arrays.asList("GET","POST"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST"));
         configuration.setAllowCredentials(true);
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
@@ -67,5 +72,10 @@ public class SecurityConfiguration {
 
     private List<String> getAllowedOrigins() {
         return Arrays.asList(this.corsAllowedOriginsRaw.split(","));
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 }
