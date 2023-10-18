6bd2e3c016760dabd6435c81c04187ff0f9607b9
mattinfern0 <mattnfernandez@gmail.com>
2023-10-17 21:38:06 -0700

    updated fonts

--- react-app/src/theme.ts
+++ react-app/src/theme.ts
@@ -14,4 +14,19 @@ export const theme = createTheme({
       },
     },
   },
+  typography: {
+    fontFamily: [
+      "Inter",
+      "-apple-system",
+      "BlinkMacSystemFont",
+      '"Segoe UI"',
+      "Roboto",
+      '"Helvetica Neue"',
+      "Arial",
+      "sans-serif",
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(","),
+  },
 });
